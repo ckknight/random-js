68dea1834b29b96fcf40965e5d312b3f86afbb75
Cameron Knight <cameron.knight@bumped.com>
2019-02-19 20:27:49 -0800

    tweak jest settings in hopes it will help older node versions

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ module.exports = {
   testEnvironment: "node",
   clearMocks: true,
   resetMocks: true,
-  restoreMocks: true
+  restoreMocks: true,
+  globals: {
+    "ts-jest": {
+      tsConfig: {
+        target: "es3",
+        module: "commonjs",
+        rootDir: "./src"
+      }
+    }
+  }
 };
